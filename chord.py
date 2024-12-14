41574002c094c868f69d1aea42695657b5d3a056
verditelabs <156155735+SuperOptimizer@users.noreply.github.com>
2024-12-14 14:03:59 -0600

    update chord score

--- chord.py
+++ chord.py
@@ -218,10 +218,10 @@ def grow_chord(start_point: int,
                 parallel_score = volume_tracker.get_parallel_score(next_pos, dp_norm)
 
                 total_score = (
-                        (strength / 255.0) * 0.3 +
-                        z_progress * 0.4 +
+                        (strength / 255.0) * 0.4 +
+                        z_progress * 0.2 +
                         #smoothness_score * 0.2 +
-                        parallel_score * 0.3
+                        parallel_score * 0.4
                 )
 
                 candidates.append((idx, next_pos, total_score, dp_norm))
